cabf3255953bd1ef4e21f1f7b4fd283e69ab9c63
Naser Afsar <nafsar@gmail.com>
2015-05-20 09:19:41 -0600

    little angularjs sample

---
+++ js/app.js
@@ -0,0 +1,21 @@
+(function(swift) {
+	'use strict';
+	var app = angular.module('swiftApp', []);
+	app.directive('swiftFeatures', function() {
+		return {
+			restrict : 'AE',
+			templateUrl : 'swift.html',
+			controller : function($scope, $http) {
+				this.image = "images/swift.jpg";
+				$http.get('js/swift.json').success(function(data) {
+					$scope.swiftdata = data;
+					console.log("data=" + JSON.stringify($scope.swiftdata[0].title));
+				}).error(function(myData) {
+					console.log("Problems=" + myData);
+				});
+			},
+			controllerAs : "ctrl"
+		};
+	});
+
+})(window.swift);
